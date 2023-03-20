0338cd2d1f8d29ab1d7690a7584b21fe00b9d83e
SM <s8subram@ucsd.edu>
2023-03-20 14:50:32 -0700

    Formatting

--- Project/project3/Ctrl.sv
+++ Project/project3/Ctrl.sv
@@ -15,7 +15,7 @@ module Ctrl (
                 Reg_From_ALU,
                 Reg_From_Mem,
                 Reg_From_Acc,
-                Acc_Write_En,	    // Acculator control
+                Acc_Write_En,	    // Accumulator control
                 Acc_From_Reg,
                 Acc_From_ALU,
                 Acc_From_Imm,
@@ -40,26 +40,26 @@ always_comb begin
   zero_init_flags;
 
   case(Instruction[8:4])
-    AND:        begin ALU_Opcode = 5'b00000;    Reg_Write_En = 1; Reg_From_ALU = 1;   end
-    ORR:        begin ALU_Opcode = 5'b00001;    Reg_Write_En = 1; Reg_From_ALU = 1;   end    
-    XOR_B:      begin ALU_Opcode = 5'b00010;  Reg_Write_En = 1; Reg_From_ALU = 1;     end
-    XOR_G:      begin ALU_Opcode = 5'b00011;  Reg_Write_En = 1; Reg_From_ALU = 1;     end
-    ADD:        begin ALU_Opcode = 5'b00100;    Reg_Write_En = 1; Reg_From_ALU = 1;   end
-	  STR:        begin                      Mem_Write_En = 1;                          end
-    LDR:        begin                      Reg_Write_En = 1; Reg_From_Mem = 1;        end
-    CMP:        begin ALU_Opcode = 5'b00111;    Acc_Write_En = 1; Acc_From_ALU = 1;   end
-    STA:        begin                      Reg_Write_En = 1; Reg_From_Acc = 1;        end
-    LDA:        begin                      Acc_Write_En = 1; Acc_From_Reg = 1;        end
-	  CMP_LS:     begin ALU_Opcode = 5'b01011;    Acc_Write_En = 1; Acc_From_ALU = 1;   end
-    SHL:        begin ALU_Opcode = 5'b01100;    Reg_Write_En = 1; Reg_From_ALU = 1;   end
-    SHR:        begin ALU_Opcode = 5'b01101;    Reg_Write_En = 1; Reg_From_ALU = 1;   end
-    SET_H:      begin Acc_Write_En = 1; Acc_Load_Hi = 1; Acc_From_Imm = 1;            end
-    SET_L:      begin Acc_Write_En = 1; Acc_Load_Hi = 0; Acc_From_Imm = 1;            end
-    BEQ:        begin LUT_Read_En = 1;  PC_Beq_Flag = AccInput == 8'b1;               end // Imm_in = Instruction[3:0] 
-    JMP:        begin LUT_Read_En = 1;  PC_Jmp_Flag = 1;                              end // Imm_in = Instruction[3:0]     
-    HLT:        begin Ack = 1;                                                        end
-    LD_LUT_H:   begin LUT_Write_En = 1; LUT_Load_Hi = 1;                              end
-    LD_LUT_L:   begin LUT_Write_En = 1; LUT_Load_Hi = 0;                              end
+    AND:        begin ALU_Opcode = 5'b00000;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    ORR:        begin ALU_Opcode = 5'b00001;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    XOR_B:      begin ALU_Opcode = 5'b00010;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    XOR_G:      begin ALU_Opcode = 5'b00011;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    ADD:        begin ALU_Opcode = 5'b00100;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+	  STR:        begin                                 Mem_Write_En = 1;                     end
+    LDR:        begin                                 Reg_Write_En = 1;  Reg_From_Mem = 1;  end
+    CMP:        begin ALU_Opcode = 5'b00111;          Acc_Write_En = 1;  Acc_From_ALU = 1;  end
+    STA:        begin                                 Reg_Write_En = 1;  Reg_From_Acc = 1;  end
+    LDA:        begin                                 Acc_Write_En = 1;  Acc_From_Reg = 1;  end
+	  CMP_LS:     begin ALU_Opcode = 5'b01011;          Acc_Write_En = 1;  Acc_From_ALU = 1;  end
+    SHL:        begin ALU_Opcode = 5'b01100;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    SHR:        begin ALU_Opcode = 5'b01101;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    SET_H:      begin Acc_Load_Hi = 1;                Acc_Write_En = 1;  Acc_From_Imm = 1;  end
+    SET_L:      begin Acc_Load_Hi = 0;                Acc_Write_En = 1;  Acc_From_Imm = 1;  end
+    LD_LUT_H:   begin LUT_Load_Hi = 1;                LUT_Write_En = 1;                     end
+    LD_LUT_L:   begin LUT_Load_Hi = 0;                LUT_Write_En = 1;                     end
+    BEQ:        begin PC_Beq_Flag = AccInput == 8'b1; LUT_Read_En = 1;                      end 
+    JMP:        begin PC_Jmp_Flag = 1;                LUT_Read_En = 1;                      end
+    HLT:        begin Ack = 1;                                                              end
     default: zero_init_flags; 
   endcase
   
