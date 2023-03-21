77c7edb319178e2a81c2790c88d2d3c9f3b91ddb
SM <s8subram@ucsd.edu>
2023-03-20 17:28:05 -0700

    Fixed Ctrl to be compliant with renamed commands

--- Project/project3/Ctrl.sv
+++ Project/project3/Ctrl.sv
@@ -35,33 +35,30 @@ always_comb begin
   zero_init_flags;
 
   case(Instruction[8:4])
-    AND:        begin ALU_Opcode = 5'b00000;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
-    ORR:        begin ALU_Opcode = 5'b00001;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
-    XOR_B:      begin ALU_Opcode = 5'b00010;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
-    XOR_G:      begin ALU_Opcode = 5'b00011;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
-    ADD:        begin ALU_Opcode = 5'b00100;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
-    CMP:        begin ALU_Opcode = 5'b00111;          Acc_Write_En = 1;  Acc_From_ALU = 1;  end
-	  CMP_LS:     begin ALU_Opcode = 5'b01011;          Acc_Write_En = 1;  Acc_From_ALU = 1;  end
-    LSL:        begin ALU_Opcode = 5'b01100;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
-    LSR:        begin ALU_Opcode = 5'b01101;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
-	  STR:        begin                                 Mem_Write_En = 1;                     end
+    ORR:        begin ALU_Opcode = 5'b00000;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    XOR_B:      begin ALU_Opcode = 5'b00001;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    XOR_G:      begin ALU_Opcode = 5'b00010;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    AND:        begin ALU_Opcode = 5'b00011;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    STR:        begin                                 Mem_Write_En = 1;                     end
     LDR:        begin                                 Reg_Write_En = 1;  Reg_From_Mem = 1;  end
     STA:        begin                                 Reg_Write_En = 1;  Reg_From_Acc = 1;  end
     LDA:        begin                                 Acc_Write_En = 1;  Acc_From_Reg = 1;  end
-    SET_H:      begin Acc_Load_Hi = 1;                Acc_Write_En = 1;  Acc_From_Imm = 1;  end
-    SET_L:      begin Acc_Load_Hi = 0;                Acc_Write_En = 1;  Acc_From_Imm = 1;  end
-    LD_LUT_H:   begin LUT_Load_Hi = 1;                LUT_Write_En = 1;                     end
     LD_LUT_L:   begin LUT_Load_Hi = 0;                LUT_Write_En = 1;                     end
-    BEQ:        begin PC_Beq_Flag = AccInput == 8'b1; LUT_Read_En = 1;                      end 
+    LD_LUT_H:   begin LUT_Load_Hi = 1;                LUT_Write_En = 1;                     end
+    SET_L:      begin Acc_Load_Hi = 0;                Acc_Write_En = 1;  Acc_From_Imm = 1;  end
+    SET_H:      begin Acc_Load_Hi = 1;                Acc_Write_En = 1;  Acc_From_Imm = 1;  end
+    CMP:        begin ALU_Opcode = 5'b01100;          Acc_Write_En = 1;  Acc_From_ALU = 1;  end
+    CMP_LS:     begin ALU_Opcode = 5'b01101;          Acc_Write_En = 1;  Acc_From_ALU = 1;  end
+    LSL:        begin ALU_Opcode = 5'b01110;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    LSR:        begin ALU_Opcode = 5'b01111;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    ADD:        begin ALU_Opcode = 5'b10000;          Reg_Write_En = 1;  Reg_From_ALU = 1;  end
+    BEQ:        begin PC_Beq_Flag = AccInput == 8'b1; LUT_Read_En = 1;                      end
     JMP:        begin PC_Jmp_Flag = 1;                LUT_Read_En = 1;                      end
     HLT:        begin Ack = 1;                                                              end
-    default: zero_init_flags; 
+    default: zero_init_flags;
   endcase
 end
 
-`ifndef __ICARUS__
-always_comb
-  op_mnemonic = op_mne'(ALU_Opcode);            // displays operation name in waveform viewer
-`endif
+ifndef __ICARUS__ always_comb op_mnemonic = op_mne'(ALU_Opcode); // displays operation name in waveform viewer endif
 
 endmodule
